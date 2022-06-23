e16fa50482f7b19926da2d8d304c562516487a5e
inord777dev <inord777dev@gmail.com>
2022-06-23 16:16:05 +0300

    feat: move mouse

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import Jimp from 'jimp';
+// import Jimp from 'jimp';
 import { httpServer } from './src/http_server/index.js';
-// import robot from 'robotjs';
+import robot from 'robotjs';
 import { WebSocketServer } from 'ws';
 
 const HTTP_PORT = 3000;
 const WS_PORT = 8000;
+const SCREEN_SIZE = 200;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
@@ -12,6 +13,23 @@ httpServer.listen(HTTP_PORT);
 console.log(`Start web socket server on the ${WS_PORT} port!`);
 const wsServer = new WebSocketServer({ port: WS_PORT });
 wsServer.on('connection', onConnect);
+
+function getPos(deltaX = 0, deltaY = 0, deltaWidth = 0, deltaHeight = 0) {
+  const mousePos = robot.getMousePos();
+  const x = Math.min(mousePos.x + deltaX, 0);
+  const y = Math.min(mousePos.y + deltaY, 0);
+
+  const screenSize = robot.getScreenSize();
+  const width = Math.min(mousePos.x + deltaWidth, screenSize.width);
+  const height = Math.min(mousePos.y + deltaHeight, screenSize.height);
+
+  return { x, y, width, height };
+}
+
+function parseDelta(value) {
+  const delta = Number.parseInt(value);
+  return Number.isNaN(delta) ? 0 : delta;
+}
 
 function onConnect(wsClient) {
   console.log('Новый пользователь');
@@ -21,20 +39,27 @@ function onConnect(wsClient) {
     console.log(commad);
     switch (commad[0]) {
       case 'mouse_up': {
+        const mousePos = robot.getMousePos();
+        robot.moveMouse(mousePos.x, mousePos.y - parseDelta(commad[1]));
         break;
       }
       case 'mouse_down': {
+        const mousePos = robot.getMousePos();
+        robot.moveMouse(mousePos.x, mousePos.y + parseDelta(commad[1]));
         break;
       }
       case 'mouse_left': {
+        const mousePos = robot.getMousePos();
+        robot.moveMouse(mousePos.x - parseDelta(commad[1]), mousePos.y);
         break;
       }
       case 'mouse_right': {
+        const mousePos = robot.getMousePos();
+        robot.moveMouse(mousePos.x + parseDelta(commad[1]), mousePos.y);
         break;
       }
       case 'mouse_position': {
-        const x = 100;
-        const y = 200;
+        const { x, y } = robot.getMousePos();
         wsClient.send(`mouse_position ${x},${y}`);
         break;
       }
@@ -48,8 +73,16 @@ function onConnect(wsClient) {
         break;
       }
       case 'prnt_scrn': {
-        const buffer = '';
-        wsClient.send(new Buffer.from(buffer).toString('base64'));
+        const mousePos = robot.getMousePos();
+        const x = Math.min(mousePos.x, 0);
+        const y = Math.min(mousePos.y, 0);
+
+        const screenSize = robot.getScreenSize();
+        const width = Math.min(mousePos.x + SCREEN_SIZE, screenSize.width);
+        const height = Math.min(mousePos.y + SCREEN_SIZE, screenSize.height);
+
+        const bitmap = robot.screen.capture(x, y, width, height);
+        wsClient.send(bitmap.image.toString('base64'));
         break;
       }
     }
